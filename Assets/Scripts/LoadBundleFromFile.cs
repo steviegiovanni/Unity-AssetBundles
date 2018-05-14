027376fe1d2b4f05c78e67d5996d4c3653de8c45
ButterCroissant\steviegiovanni <steviegiovanni@gmail.com>
2018-05-15 05:08:57 +0800

    try testscene assetbundle

---
+++ Assets/Scripts/LoadBundleFromFile.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.IO;
+
+public class LoadBundleFromFile : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		AssetBundle myLoadedAssetBundle = AssetBundle.LoadFromFile(Path.Combine(Application.streamingAssetsPath, "testscene"));
+		if (myLoadedAssetBundle == null) {
+			Debug.Log("Failed to load AssetBundle!");
+			return;
+		}
+		SceneManager.LoadScene ("testscene");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
