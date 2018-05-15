bb3a10289987bea4faf811d4ac1d5be65f61a000
ButterCroissant\steviegiovanni <steviegiovanni@gmail.com>
2018-05-16 05:56:08 +0800

    - tried separating fsm to another bundle - tried reading bundle manifest and loading all dependencies before loading unit1 bundle

--- Assets/Scripts/LoadUnitBundle.cs
+++ Assets/Scripts/LoadUnitBundle.cs
@@ -6,20 +6,23 @@ using System.IO;
 public class LoadUnitBundle : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
-		/*AssetBundle stateMachineBehavioursBundle = AssetBundle.LoadFromFile(Path.Combine(Application.streamingAssetsPath, "statemachinebehaviours"));
-		if (stateMachineBehavioursBundle == null) {
-			Debug.Log ("Failed to load behaviour bundle!");
+		AssetBundle assetBundle = AssetBundle.LoadFromFile(Path.Combine(Application.streamingAssetsPath,"AssetBundles"));
+		AssetBundleManifest manifest = assetBundle.LoadAsset<AssetBundleManifest>("AssetBundleManifest");
+		string[] dependencies = manifest.GetAllDependencies("units/unit1"); //Pass the name of the bundle you want the dependencies for.
+		foreach(string dependency in dependencies)
+		{
+			AssetBundle.LoadFromFile(Path.Combine(Application.streamingAssetsPath, dependency));
+		}
+
+
+		AssetBundle unitAssetBundle = AssetBundle.LoadFromFile(Path.Combine(Application.streamingAssetsPath, "units/unit1"));
+		if (unitAssetBundle == null) {
+			Debug.Log ("Failed to load unit bundle!");
 			return;
-		} else {*/
-			AssetBundle unitAssetBundle = AssetBundle.LoadFromFile(Path.Combine(Application.streamingAssetsPath, "units/unit1"));
-			if (unitAssetBundle == null) {
-				Debug.Log ("Failed to load unit bundle!");
-				return;
-			} else {
-				GameObject unit1prefab = unitAssetBundle.LoadAsset<GameObject> ("unit1");
-				GameObject unit1 = Instantiate (unit1prefab);
-			}
-		//}
+		} else {
+			GameObject unit1prefab = unitAssetBundle.LoadAsset<GameObject> ("unit1");
+			GameObject unit1 = Instantiate (unit1prefab);
+		}
 	}
 
 	// Update is called once per frame
